882af30a96b81acafc620acd819ba5b6e0793a76
unclay <wcl58981298@gmail.com>
2016-12-19 15:00:00 +0800

    修改开发内容，用于测试浏览器的单元测试

--- template/src/index.js
+++ template/src/index.js
@@ -1,3 +1,7 @@
 module.exports = function () {
+	// document用于测试浏览器端
+	let div = document.createElement('div')
+	div.innerHTML = 'Hello world'
+	document.body.appendChild(div)
   return 'Hello World'
 }
